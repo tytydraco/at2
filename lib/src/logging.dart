955cefb9b2fc3a329f2aa7fae634078bdd991fa1
Tyler Nijmeh <tylernij@gmail.com>
2025-11-17 14:43:10 -0800

    Add logging
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/logging.dart
+++ lib/src/logging.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 /// The log level for [log].
 enum LogLevel {
   /// Information that is useful to developers.
@@ -15,5 +17,18 @@ enum LogLevel {
 
 /// Log something to the standard output and standard error.
 void log(String content, {LogLevel logLevel = LogLevel.log}) {
-
+  switch (logLevel) {
+    case LogLevel.debug: {
+      stdout.writeln('\x1B[38;5;244m$content\x1B[0m');
+    }
+    case LogLevel.log: {
+      stdout.writeln('\x1B[30m$content\x1B[0m');
+    }
+    case LogLevel.warn: {
+      stdout.writeln('\x1B[33m$content\x1B[0m');
+    }
+    case LogLevel.error: {
+      stdout.writeln('\x1B[31m$content\x1B[0m');
+    }
+  }
 }
---
+++ test/src/test_logging.dart
@@ -0,0 +1,22 @@
+import 'package:at2/src/logging.dart';
+import 'package:test/scaffolding.dart';
+
+Future<void> main() async {
+    group('Logging tests', () {
+      test('Debug log', () {
+        log('test debug', logLevel: LogLevel.debug);
+      });
+
+      test('Log log', () {
+        log('test log', logLevel: LogLevel.log);
+      });
+
+      test('Warning log', () {
+        log('test warning', logLevel: LogLevel.warn);
+      });
+
+      test('Error log', () {
+        log('test error', logLevel: LogLevel.error);
+      });
+    });
+}
