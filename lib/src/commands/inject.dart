87189d97399c9a2299057e199756531fc489844f
Tyler Nijmeh <tylernij@gmail.com>
2025-11-17 15:35:49 -0800

    inject: Flesh out more of the skeleton
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/commands/inject.dart
+++ lib/src/commands/inject.dart
@@ -1,7 +1,27 @@
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
+import 'package:at2/src/logging.dart';
 
 /// The inject command.
 class InjectCommand extends Command<void> {
+  /// Creates a new [InjectCommand].
+  InjectCommand() {
+    argParser
+      ..addOption(
+        'directory',
+        abbr: 'd',
+        help: 'The working tree.',
+        defaultsTo: 'working',
+      )
+      ..addOption(
+        'input',
+        abbr: 'i',
+        help: 'The .riv file to inject.',
+        defaultsTo: 'input.riv',
+      );
+  }
+
   @override
   String get name => 'inject';
 
@@ -10,6 +30,16 @@ class InjectCommand extends Command<void> {
 
   @override
   Future<void> run() async {
-    // TODO: Implement inject command logic
+    final directory = argResults?['directory'] as String?;
+    final input = argResults?['input'] as String?;
+
+    // Use the directory option
+    if (directory != null) {
+      log('Injecting $input into directory: $directory');
+      // TODO(tytydraco): IMPLEMENT INJECT FUNCTIONALITY
+    } else {
+      log(argParser.usage);
+      exit(64);
+    }
   }
 }
