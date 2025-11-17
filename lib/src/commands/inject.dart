cb83353d728ed87b4a04232f21df888a8fef82b8
Tyler Nijmeh <tylernij@gmail.com>
2025-11-17 15:52:29 -0800

    inject: Fully implement
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/commands/inject.dart
+++ lib/src/commands/inject.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:args/command_runner.dart';
 import 'package:at2/src/logging.dart';
+import 'package:path/path.dart' as path;
 
 /// The inject command.
 class InjectCommand extends Command<void> {
@@ -22,24 +23,54 @@ class InjectCommand extends Command<void> {
       );
   }
 
+  /// The .riv file path for the working Android project.
+  static const rivPathAndroid = 'android/app/src/main/res/raw/input.riv';
+
+  /// The .riv file path for the working iOS project.
+  static const rivPathIOS = 'ios/input.riv';
+
+  /// The .riv file path for the working Flutter project.
+  static const rivPathFlutter = 'flutter/assets/input.riv';
+
   @override
   String get name => 'inject';
 
   @override
   String get description => 'Inject a .riv into the working tree.';
 
+  Future<void> _inject(File input, String destinationPath) async {
+    try {
+      await input.copy(destinationPath);
+    } on Exception catch (_) {
+      log(
+        'Failed to copy ${input.path} -> $destinationPath',
+        logLevel: LogLevel.warn,
+      );
+    }
+  }
+
   @override
   Future<void> run() async {
-    final directory = argResults?['directory'] as String?;
-    final input = argResults?['input'] as String?;
+    final directoryPath = argResults?['directory'] as String;
+    final inputPath = argResults?['input'] as String;
 
-    // Use the directory option
-    if (directory != null) {
-      log('Injecting $input into directory: $directory');
-      // TODO(tytydraco): IMPLEMENT INJECT FUNCTIONALITY
-    } else {
-      log(argParser.usage);
-      exit(64);
+    final directory = Directory(directoryPath);
+    final input = File(inputPath);
+
+    // Assert directory exists.
+    if (!directory.existsSync()) {
+      log('Directory does not exist', logLevel: LogLevel.error);
+      exit(1);
     }
+
+    // Assert input exists.
+    if (!input.existsSync()) {
+      log('Input does not exist', logLevel: LogLevel.error);
+      exit(1);
+    }
+
+    await _inject(input, path.join(directory.path, rivPathAndroid));
+    await _inject(input, path.join(directory.path, rivPathIOS));
+    await _inject(input, path.join(directory.path, rivPathFlutter));
   }
 }
