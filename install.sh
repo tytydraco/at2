34c1ab6fc4fb6bf22574ed81cb22e975c1f3692c
Tyler Nijmeh <tylernij@gmail.com>
2025-11-20 08:38:52 -0800

    install: Fix not updating
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
+# Ensure dependencies are up to date
+dart pub get
+
+# Deactivate existing installation
+dart pub global deactivate at2 2>/dev/null || true
+
+# Remove the actual binary script wrapper
+rm -f "$HOME/.pub-cache/bin/at2"
+
+# Remove the snapshot file that the wrapper references
+# This is in the project's .dart_tool directory
+rm -rf .dart_tool/pub/bin/at2
+
+# Remove the package cache
+rm -rf "$HOME/.pub-cache/global_packages/at2"
+
+# Activate with path source, forcing overwrite
+# This will regenerate the snapshot
 dart pub global activate -s path . --overwrite
